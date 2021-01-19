a005cd40b1d1d2bd00af56ef49ae287dad41663b
grzegorz szopinski <szopinski.grzegorz@gmail.com>
2021-01-20 00:36:29 +0100

    Add basic backprop

--- tensor.py
+++ tensor.py
@@ -2,11 +2,26 @@ import numpy as np
 
 
 class Tensor(object):
-    def __init__(self, data):
+    """
+    Basic class to represent generic tensor.
+    It supports following (element-wise) operations:
+    * addition
+    """
+    def __init__(self, data, creators=None, creation_op=None):
         self.data = np.array(data)
+        self.creators = creators
+        self.creation_op = creation_op
+        self.grad = None
+
+    def backward(self, grad):
+        self.grad = grad
+
+        if self.creation_op == "add":
+            self.creators[0].backward(grad)
+            self.creators[1].backward(grad)
 
     def __add__(self, other):
-        return Tensor(self.data + other.data)
+        return Tensor(self.data + other.data, creators=[self, other], creation_op="add")
 
     def __repr__(self):
         return str(self.data.__repr__())
