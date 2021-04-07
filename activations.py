229b9110d14bd287c47b622c1d9b08671f7d46f6
grzegorz szopinski <szopinski.grzegorz@gmail.com>
2021-04-07 20:05:17 +0200

    add basic activation functions

---
+++ activations.py
@@ -0,0 +1,44 @@
+import numpy as np
+from nn import Operation, ParamOperation
+
+
+class Linear(Operation):
+    """
+    "Identity" activation function
+    """
+
+    def __init__(self) -> None:
+        """Pass"""
+        super().__init__()
+
+    def _output(self) -> np.ndarray:
+        """Pass through"""
+        return self.input_
+
+    def _input_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        """Pass through"""
+        return output_grad
+
+
+class Sigmoid(Operation):
+    """
+    Sigmoid activation function.
+    """
+
+    def __init__(self) -> None:
+        """Pass"""
+        super().__init__()
+
+    def _output(self) -> np.ndarray:
+        """
+        Compute output.
+        """
+        return 1.0 / (1.0 + np.exp(-1.0 * self.input_))
+
+    def _input_grad(self, output_grad: np.ndarray) -> np.ndarray:
+        """
+        Compute input gradient.
+        """
+        sigmoid_backward = self.output * (1.0 - self.output)
+        input_grad = sigmoid_backward * output_grad
+        return input_grad
